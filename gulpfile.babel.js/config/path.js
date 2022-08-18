7693ce7cd6614d33e963c50de305cd641901707f
EVALORE <vladimireremia377@gmail.com>
2022-08-18 12:33:55 +0300

    added header & some new changes

--- gulpfile.babel.js/config/path.js
+++ gulpfile.babel.js/config/path.js
@@ -31,12 +31,12 @@ export default {
 
     js: {
         src: pathSrc + "/js/*.js",
-        watch: pathSrc + "/js/**/*.js",
+        watch: pathSrc + "/js/**/*.{js, json}",
         dest: pathDest + "/js"
     },
 
     img: {
-        src: pathSrc + "/img/*.{png,jpg,jpeg,gif,svg}",
+        src: pathSrc + "/img/**/*.{png,jpg,jpeg,gif,svg}",
         watch: pathSrc + "/img/**/*.{png,jpg,jpeg,gif,svg}",
         dest: pathDest + "/img"
     },
