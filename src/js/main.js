cd83e1ce3eace54a4c72ea18a0e2a7997056b874
EVALORE <vladimireremia377@gmail.com>
2022-09-15 20:24:55 +0300

    add pages
    add about page
    add card page
    add category pages
    add user page
    add registration & login pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,3 @@
-// scroll
-const anchors = document.querySelectorAll('a[href*="#"]')
-
-for (let anchor of anchors) {
-    anchor.addEventListener('click', function(event) {
-        event.preventDefault();
-        const blockID = anchor.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: "smooth",
-            block: 'start'
-        })
-    })
-}
-
-
-
-
-
-
-
-
-
 if (window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = function (callback, thisArg) {
         thisArg = thisArg || window;
@@ -35,13 +13,13 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
     const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
     const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden')
-    
+
     dropDownBtn.addEventListener('click', function () {
         dropDownList.classList.toggle('dropdown__list--visible');
         this.classList.add('dropdown__button--active');
-    
+
     });
-    
+
     dropDownListItems.forEach(function(listItem) {
         listItem.addEventListener('click', function (e) {
             e.stopPropagation();
@@ -51,14 +29,14 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
             dropDownList.classList.remove('dropdown__list--visible');
         })
     });
-    
+
     document.addEventListener('click', function(e) {
         if ( e.target !== dropDownBtn ) {
             dropDownBtn.classList.remove('dropdown__button--active');
             dropDownList.classList.remove('dropdown__list--visible');
         }
     });
-    
+
     document.addEventListener('keydown', function (e) {
         if ( e.key === 'Tab' || e.key === 'Escape' ) {
             dropDownBtn.classList.remove('dropdown__button--active');
@@ -66,4 +44,4 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
         }
     });
 
-});
+}); 
