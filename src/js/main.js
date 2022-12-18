9d1c4499a2c9429bd4ea44e41b89b16452f95364
EVALORE <vladimireremia377@gmail.com>
2022-12-18 15:18:42 +0200

    feat: init & feat
    init: start filter task
    feat: implement custom radio checkbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,52 @@
-import { carousel } from "./module/carousel";
+import {carousel} from './module/carousel'
+import {range} from './module/range-slider'
 
-carousel();
+range()
+carousel()
 
 if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, thisArg) {
-        thisArg = thisArg || window;
-        for (var i = 0; i < this.length; i++) {
-            callback.call(thisArg, this[i], i, this);
-        }
-    };
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this)
+    }
+  }
 }
 
 document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+  const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button')
+  const dropDownList = dropDownWrapper.querySelector('.dropdown__list')
+  const dropDownListItems = dropDownList.querySelectorAll(
+    '.dropdown__list-item'
+  )
+  const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden')
 
-    const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
-    const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
-    const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
-    const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden')
+  dropDownBtn.addEventListener('click', function () {
+    dropDownList.classList.toggle('dropdown__list--visible')
+    this.classList.add('dropdown__button--active')
+  })
 
-    dropDownBtn.addEventListener('click', function () {
-        dropDownList.classList.toggle('dropdown__list--visible');
-        this.classList.add('dropdown__button--active');
+  dropDownListItems.forEach(function (listItem) {
+    listItem.addEventListener('click', function (e) {
+      e.stopPropagation()
+      dropDownBtn.innerText = this.innerText
+      dropDownBtn.focus()
+      dropDownInput.value = this.dataset.value
+      dropDownList.classList.remove('dropdown__list--visible')
+    })
+  })
 
-    });
+  document.addEventListener('click', function (e) {
+    if (e.target !== dropDownBtn) {
+      dropDownBtn.classList.remove('dropdown__button--active')
+      dropDownList.classList.remove('dropdown__list--visible')
+    }
+  })
 
-    dropDownListItems.forEach(function(listItem) {
-        listItem.addEventListener('click', function (e) {
-            e.stopPropagation();
-            dropDownBtn.innerText = this.innerText;
-            dropDownBtn.focus();
-            dropDownInput.value = this.dataset.value;
-            dropDownList.classList.remove('dropdown__list--visible');
-        })
-    });
-
-    document.addEventListener('click', function(e) {
-        if ( e.target !== dropDownBtn ) {
-            dropDownBtn.classList.remove('dropdown__button--active');
-            dropDownList.classList.remove('dropdown__list--visible');
-        }
-    });
-
-    document.addEventListener('keydown', function (e) {
-        if ( e.key === 'Tab' || e.key === 'Escape' ) {
-            dropDownBtn.classList.remove('dropdown__button--active');
-            dropDownList.classList.remove('dropdown__list--visible');
-        }
-    });
-
-}); 
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Tab' || e.key === 'Escape') {
+      dropDownBtn.classList.remove('dropdown__button--active')
+      dropDownList.classList.remove('dropdown__list--visible')
+    }
+  })
+})
